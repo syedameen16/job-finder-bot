d3232671e02abdb258320600d67ddda598b99d39
Syed Ameen <109243278+syedameen16@users.noreply.github.com>
2026-02-02 11:43:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-import requests
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
-
-print("BOT_TOKEN:", BOT_TOKEN)
-print("CHAT_ID:", CHAT_ID)
-
-url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-data = {"chat_id": CHAT_ID, "text": "Test from GitHub"}
-
-r = requests.post(url, data=data)
-
-print("Status Code:", r.status_code)
-print("Response:", r.text)
+print("BOT_TOKEN:", os.getenv("BOT_TOKEN"))
+print("CHAT_ID:", os.getenv("CHAT_ID"))
