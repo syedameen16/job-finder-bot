85bc11a37f4c657852e1a4c7a428378c9ca1d17b
Syed Ameen <109243278+syedameen16@users.noreply.github.com>
2026-02-02 11:11:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,20 +6,9 @@ CHAT_ID = os.getenv("CHAT_ID")
 
 def send_telegram(msg):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    data = {
-        "chat_id": CHAT_ID,
-        "text": msg
-    }
+    data = {"chat_id": CHAT_ID, "text": msg}
     r = requests.post(url, data=data)
-    print(r.text)  # this will show error in GitHub logs if any
-
-def check_jobs():
-    jobs = [
-        "Data Analyst | SQL Excel | Bangalore",
-        "MIS Executive | Reporting | Chennai"
-    ]
-    for job in jobs:
-        send_telegram(job)
+    print(r.text)
 
 if __name__ == "__main__":
-    check_jobs()
+    send_telegram("Bot test message from GitHub Actions")
