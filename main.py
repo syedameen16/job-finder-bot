702ff422713bec85e79edf687c8bc305709b70d9
Syed Ameen <109243278+syedameen16@users.noreply.github.com>
2026-02-02 10:44:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import requests
-import time
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -7,27 +6,20 @@ CHAT_ID = os.getenv("CHAT_ID")
 
 def send_telegram(msg):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    data = {"chat_id": CHAT_ID, "text": msg}
-    requests.post(url, data=data)
+    data = {
+        "chat_id": CHAT_ID,
+        "text": msg
+    }
+    r = requests.post(url, data=data)
+    print(r.text)  # this will show error in GitHub logs if any
 
 def check_jobs():
-    # TEMP demo — replace with real scraping later
     jobs = [
         "Data Analyst | SQL Excel | Bangalore",
         "MIS Executive | Reporting | Chennai"
     ]
     for job in jobs:
         send_telegram(job)
-        
-        
+
 if __name__ == "__main__":
     check_jobs()
-
-# while True:
-#     check_jobs()
-#     time.sleep(3600)
-    # Check every hour
-    
-
-
-    
